4938ddd5a1beb6bf39c2ad0c95ec02f529771355
Cygnus <cpnarido@yahoo.com>
2024-02-21 17:54:42 +0800

    added script for step 3
    added script for TC_ACTIVATE_003

--- test/pageobjects/dashboard-page.ts
+++ test/pageobjects/dashboard-page.ts
@@ -1,4 +1,4 @@
-import { $ } from "@wdio/globals";
+import { $, $$ } from "@wdio/globals";
 import config from "../../config/config";
 const data = config.data;
 const url = config.urls.urls;
@@ -35,6 +35,53 @@ class dashboardPage {
     return textExists;
   }
 
+  async verifyErrorText(field: string) {
+    const textData = await $(
+      `//label[text()="${field}"]/../parent::div//p[contains(@class,"form-error")]`
+    );
+    const textExist: boolean = await textData.isExisting();
+    if (textExist) {
+      const errorText = textData.getText();
+      return errorText;
+    } else {
+      return "No error.";
+    }
+  }
+
+  async verifyPreferredBusinessHandleError(field: string) {
+    const textData = await $(
+      `//label[text()="${field}"]/..//following-sibling::div//p`
+    );
+    const textExist: boolean = await textData.isExisting();
+    if (textExist) {
+      const errorText = textData.getText();
+      return errorText;
+    } else {
+      return "No error.";
+    }
+  }
+
+  async verifyMultipleErrorText(field: string) {
+    const textData = await $$(
+      `//label[text()="${field}"]/../parent::div//p[contains(@class,"form-error")]`
+    );
+    let errors: Array<string> = [];
+    if (textData.length === 0) {
+      errors = ["No error."];
+      return errors;
+    }
+    for (const element of textData) {
+      const elementExists = await element.isExisting();
+      if (elementExists) {
+        const value = await element.getText();
+        errors.push(value);
+      } else {
+        errors.push("No error.");
+      }
+    }
+    return errors;
+  }
+
   async clickButton(label: string) {
     const nextButton = await $(
       `//span[text()="${label}"]/parent::div/parent::button`
@@ -42,6 +89,104 @@ class dashboardPage {
     await nextButton.waitForDisplayed();
     await nextButton.click();
     await browser.pause(1000);
+  }
+
+  async emptyAllFields(fields: any) {
+    for (const field in fields) {
+      if (fields.hasOwnProperty(field)) {
+        const fieldData = fields[field];
+        if (field == "business_website" || field == "product_description") {
+          let element: any;
+          if (field == "business_website") {
+            element = await $(`//*[@id="${fieldData.name}"]//input`);
+          } else if (field == "product_description") {
+            element = await $(`//*[@id="${fieldData.name}"]`);
+          }
+          await element.click();
+          const websiteText: any = await element.getValue();
+          for (const letter in websiteText) {
+            browser.keys("\uE003");
+          }
+        } else {
+          const textField = await $(`//*[@id="${fieldData.name}"]`);
+          await textField.clearValue();
+        }
+      }
+    }
+  }
+
+  async scanErrors(fields: any) {
+    const fieldCount = Object.keys(fields).length;
+    let errorCounter = 0;
+    for (const field in fields) {
+      if (fields.hasOwnProperty(field)) {
+        const fieldData = fields[field];
+        const errorCheck = await $(`//p[text()="${fieldData.blank_error}"]`);
+        const errorMessageExists: boolean = await errorCheck.isExisting();
+        if (errorMessageExists) {
+          errorCounter++;
+        }
+      }
+    }
+    if (errorCounter == fieldCount) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async clickSpan(text: string) {
+    const spanElement = await $(`//span[text()="${text}"]`);
+    await spanElement.waitForDisplayed();
+    await spanElement.click();
+  }
+
+  async setFieldValue(fields: any, field: string, value: any) {
+    if (field == "business_website" || field == "product_description") {
+      let element: any;
+      if (field == "business_website") {
+        element = await $(`//*[@id="${fields[field].name}"]//input`);
+      } else if (field == "product_description") {
+        element = await $(`//*[@id="${fields[field].name}"]`);
+      }
+      await element.click();
+      const websiteText: any = await element.getValue();
+      for (const letter in websiteText) {
+        browser.keys("\uE003");
+      }
+      for (const letter in value) {
+        browser.keys(letter);
+      }
+    } else {
+      const textField = await $(`//*[@id="${fields[field].name}"]`);
+      await textField.clearValue();
+      await textField.setValue(value);
+    }
+  }
+
+  async setProductDescriptionField(fields: any, field: string, value: any) {
+    const element = await $(`//*[@id="${fields[field].name}"]`);
+    await element.click();
+    const content: any = await element.getValue();
+    for (const letter in content) {
+      browser.keys("\uE003");
+    }
+    await browser.keys(value);
+  }
+
+  async setBusinessWebsiteField(fields: any, field: string, value: any) {
+    const element = await $(`//*[@id="${fields[field].name}"]//input`);
+    await element.click();
+    const content: any = await element.getValue();
+    for (const letter in content) {
+      browser.keys("\uE003");
+    }
+    await browser.keys(value);
+  }
+
+  async getUrlDisplayed() {
+    const pElement = await $(`//p[contains(text(),"https://pm.link/")]`);
+    return pElement.getText();
   }
 }
 
--- test/specs/signup.ts
+++ test/specs/signup.ts
@@ -38,4 +38,316 @@ describe("Account Activation", async function () {
     // Click Next button
     await dashboard.clickButton("Next");
   });
+
+  it("Verify that all validation works correctly for each field for Step 3", async function () {
+    let error: string;
+    let errors: Array<string>;
+    let data = tc_data.TC_ACTIVATE_003;
+    // Leave all the fields empty, click the Save changes as draft button (found at the bottom of the page)
+    await dashboard.emptyAllFields(data.fields);
+    await dashboard.clickButton("Next");
+
+    // Verify that the mandatory fields have error messages below their input fields
+    const allErrorsExisting: boolean = await dashboard.scanErrors(data.fields);
+    expect(allErrorsExisting).toBeTruthy;
+
+    // Set value #1 for Business store name, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "business_store_name",
+      data.fields.business_store_name.value1
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.business_store_name.label
+    );
+    expect(error).toBe(data.fields.business_store_name.value1_error);
+
+    // Set value #2 for Business store name, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "business_store_name",
+      data.fields.business_store_name.value2
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.business_store_name.label
+    );
+    expect(error).toBe(data.fields.business_store_name.value2_error);
+
+    // Set value #1 for Customer service number, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "customer_service_number",
+      data.fields.customer_service_number.value1
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.customer_service_number.label
+    );
+    expect(error).toBe(data.fields.customer_service_number.value1_error);
+
+    // Set value #2 for Customer service number, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "customer_service_number",
+      data.fields.customer_service_number.value2
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.customer_service_number.label
+    );
+    expect(error).toBe(data.fields.customer_service_number.value2_error);
+
+    // Set value #3 for Customer service number, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "customer_service_number",
+      data.fields.customer_service_number.value3
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.customer_service_number.label
+    );
+    expect(error).toBe(data.fields.customer_service_number.value3_error);
+
+    // Set value #4 for Customer service number, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "customer_service_number",
+      data.fields.customer_service_number.value4
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.customer_service_number.label
+    );
+    expect(error).toBe(data.fields.customer_service_number.value4_error);
+
+    // Set value #5 for Customer service number, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "customer_service_number",
+      data.fields.customer_service_number.value5
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.customer_service_number.label
+    );
+    expect(error).toBe(data.fields.customer_service_number.value5_error);
+
+    // Set value #6 for Customer service number, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "customer_service_number",
+      data.fields.customer_service_number.value6
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.customer_service_number.label
+    );
+    expect(error).toBe(data.fields.customer_service_number.value6_error);
+
+    // Set value #1 for Business address, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "business_address",
+      data.fields.business_address.value1
+    );
+    await dashboard.setFieldValue(
+      data.fields,
+      "city_municipality",
+      data.fields.city_municipality.value1
+    );
+    await dashboard.setFieldValue(
+      data.fields,
+      "zip_code",
+      data.fields.zip_code.value1
+    );
+    await dashboard.clickButton("Next");
+    errors = await dashboard.verifyMultipleErrorText(
+      data.fields.business_address.label
+    );
+
+    expect(errors).toContain(data.fields.business_address.value1_error);
+    expect(errors).toContain(data.fields.city_municipality.value1_error);
+    expect(errors).toContain(data.fields.zip_code.value1_error);
+
+    // Set value #2 for Business address, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "business_address",
+      data.fields.business_address.value2
+    );
+    await dashboard.setFieldValue(
+      data.fields,
+      "city_municipality",
+      data.fields.city_municipality.value2
+    );
+    await dashboard.setFieldValue(
+      data.fields,
+      "zip_code",
+      data.fields.zip_code.value2
+    );
+    await dashboard.clickButton("Next");
+    errors = await dashboard.verifyMultipleErrorText(
+      data.fields.business_address.label
+    );
+
+    expect(errors).toContain(data.fields.business_address.value2_error);
+    expect(errors).toContain(data.fields.city_municipality.value2_error);
+    expect(errors).toContain(data.fields.zip_code.value2_error);
+
+    // Set value #3 for Business address, then click Save changes as draft
+    await dashboard.setFieldValue(
+      data.fields,
+      "business_address",
+      data.fields.business_address.value3
+    );
+    await dashboard.setFieldValue(
+      data.fields,
+      "city_municipality",
+      data.fields.city_municipality.value3
+    );
+    await dashboard.setFieldValue(
+      data.fields,
+      "zip_code",
+      data.fields.zip_code.value3
+    );
+    await dashboard.clickButton("Next");
+    errors = await dashboard.verifyMultipleErrorText(
+      data.fields.business_address.label
+    );
+
+    expect(errors).toContain(data.fields.business_address.value3_error);
+    expect(errors).toContain(data.fields.city_municipality.value3_error);
+    expect(errors).toContain(data.fields.zip_code.value3_error);
+
+    // Set value #1 for Product description and nature of your business, then click Save changes as draft
+    await dashboard.setProductDescriptionField(
+      data.fields,
+      "product_description",
+      data.fields.product_description.value1
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.product_description.label
+    );
+    expect(error).toBe(data.fields.product_description.value1_error);
+
+    // Set value #2 for Product description and nature of your business, then click Save changes as draft
+    await dashboard.setProductDescriptionField(
+      data.fields,
+      "product_description",
+      data.fields.product_description.value2
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.product_description.label
+    );
+    expect(error).toBe(data.fields.product_description.value2_error);
+
+    // Set value #3 for Product description and nature of your business, then click Save changes as draft
+    await dashboard.setProductDescriptionField(
+      data.fields,
+      "product_description",
+      data.fields.product_description.value3
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(
+      data.fields.product_description.label
+    );
+    expect(error).toBe(data.fields.product_description.value3_error);
+
+    // Set value #1 for Business website or social media, then click Save changes as draft
+    await dashboard.setBusinessWebsiteField(
+      data.fields,
+      "business_website",
+      data.fields.business_website.value1
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(data.fields.business_website.label);
+    expect(error).toBe(data.fields.business_website.value1_error);
+
+    // Set value #2 for Business website or social media, then click Save changes as draft
+    await dashboard.setBusinessWebsiteField(
+      data.fields,
+      "business_website",
+      data.fields.business_website.value2
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(data.fields.business_website.label);
+    expect(error).toBe(data.fields.business_website.value2_error);
+
+    // Set value #3 for Business website or social media, then click Save changes as draft
+    await dashboard.setBusinessWebsiteField(
+      data.fields,
+      "business_website",
+      data.fields.business_website.value3
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(data.fields.business_website.label);
+    expect(error).toBe(data.fields.business_website.value3_error);
+
+    // Set value #4 for Business website or social media, then click Save changes as draft
+    await dashboard.setBusinessWebsiteField(
+      data.fields,
+      "business_website",
+      data.fields.business_website.value4
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyErrorText(data.fields.business_website.label);
+    expect(error).toBe(data.fields.business_website.value4_error);
+
+    // Set value #1 for Preferred business handle, then click Save changes as draft
+
+    await dashboard.setFieldValue(
+      data.fields,
+      "preferred_business_handle",
+      data.fields.preferred_business_handle.value1
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyPreferredBusinessHandleError(
+      data.fields.preferred_business_handle.label
+    );
+    expect(error).toBe(data.fields.preferred_business_handle.value1_error);
+
+    // Set value #2 for Preferred business handle, then click Save changes as draft
+
+    await dashboard.setFieldValue(
+      data.fields,
+      "preferred_business_handle",
+      data.fields.preferred_business_handle.value2
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyPreferredBusinessHandleError(
+      data.fields.preferred_business_handle.label
+    );
+    expect(error).toBe(data.fields.preferred_business_handle.value2_error);
+
+    // Set value #3 for Preferred business handle, then click Save changes as draft
+
+    await dashboard.setFieldValue(
+      data.fields,
+      "preferred_business_handle",
+      data.fields.preferred_business_handle.value3
+    );
+    await dashboard.clickButton("Next");
+    error = await dashboard.verifyPreferredBusinessHandleError(
+      data.fields.preferred_business_handle.label
+    );
+    expect(error).toBe(data.fields.preferred_business_handle.value3_error);
+
+    // Put any value in the Preferred business handle, check the URL below the field
+
+    await dashboard.setFieldValue(
+      data.fields,
+      "preferred_business_handle",
+      data.fields.preferred_business_handle.value3
+    );
+    const url = await dashboard.getUrlDisplayed();
+    expect(url).toBe(data.fields.preferred_business_handle.value4_expect);
+
+    // Click Next button
+    await dashboard.clickButton("Next");
+  });
 });
